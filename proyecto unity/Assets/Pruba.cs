225d74dc12810f72baaec9ba07642c5d0260dab8
Juan Manuel <bycross028@hotmail.com>
2015-01-23 14:41:15 -0300

    Script prueba

--- proyecto unity/Assets/Pruba.cs
+++ proyecto unity/Assets/Pruba.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class Pruba : MonoBehaviour {
 
 	// Use this for initialization
+
 	void Start () {
-	
+        Debug.Log("Hola");
 	}
 	
 	// Update is called once per frame
