fd3b1b8fb576c474d66c2eb1384a95425e6865fe
ezedevGame <ezequiel.guzman@live.com.ar>
2015-01-23 14:49:42 -0300

    texto2
    es solo modificacion del script

--- proyecto unity/Assets/prueba2.cs
+++ proyecto unity/Assets/prueba2.cs
@@ -9,11 +9,13 @@ public class prueba2 : MonoBehaviour {
 		for(int i = 0 ; i < 10 ; i++)
 		{
 			Debug.Log(i);
+			// e gatoooo eto es un comentario e
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		Debug.Log("hola");
 	}
 }
