03b44fd27feb80c70e4bf282c8d3a72ed4cb89e1
Juan Manuel <bycross028@hotmail.com>
2015-01-24 14:47:59 -0300

    MemoryGame

--- Proyecto Unity 2D/Assets/scripts/memoryGame/Carta.cs
+++ Proyecto Unity 2D/Assets/scripts/memoryGame/Carta.cs
@@ -1,15 +1,123 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Carta : MonoBehaviour {
+public class Carta : MonoBehaviour 
+{
+    public int id;
+    public float timeWait = 2.0f;
+    public float timeRotation = 1.0f;
+    public float timeScale = 2.0f;
+    public float rotationVelocity;
+    
+    public GameObject front;
+    public GameObject back;
+    public Sprite SpriteBack;
+    public MemoryGameMannager managger;
 
-	// Use this for initialization
-	void Start () {
-	
+    //! Private
+    private SpriteRenderer sptriteRenderFront;
+
+    public SpriteRenderer SptriteRenderFront
+    {
+        get { return sptriteRenderFront; }
+    }
+
+    private SpriteRenderer sptriteRenderBack;
+
+    public SpriteRenderer SptriteRenderBack
+    {
+        get { return sptriteRenderBack; }
+    }
+
+    private bool orientation = false;
+    private bool rotationHidde = false;
+    private bool rotationShow = false;
+    private bool scalingHidde = false;
+
+    //! Private Timer
+    private float time = 0.0f;
+
+	void Start () 
+    {
+        sptriteRenderFront = front.GetComponent<SpriteRenderer>();
+        sptriteRenderBack = back.GetComponent<SpriteRenderer>();
+
+        sptriteRenderBack.sprite = SpriteBack;
+
+        // Hiden Cart
+        Invoke("rotateHidde", timeWait);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () 
+    {
+        
+        if (gameObject.transform.rotation.eulerAngles.y < 90f && orientation)
+            toogleOrder();
+
+        if (gameObject.transform.rotation.eulerAngles.y > 90f && !orientation)
+            toogleOrder();
+
+        updateRotate();
 	}
+
+    void toogleOrder()
+    {
+        int temp = sptriteRenderFront.sortingOrder;
+        sptriteRenderFront.sortingOrder = sptriteRenderBack.sortingOrder;
+        sptriteRenderBack.sortingOrder = temp;
+
+        orientation = !orientation;
+    }
+
+    public void rotateShow()
+    {
+        time = gameObject.transform.rotation.eulerAngles.y * timeRotation / 180.0f;
+        rotationShow = true;
+    }
+
+    public void rotateHidde()
+    {
+        time = timeRotation - gameObject.transform.rotation.eulerAngles.y * timeRotation / 180.0f;
+        rotationHidde = true;
+    }
+
+    public void scaleHidde()
+    {
+        time = timeScale - gameObject.transform.localScale.x * timeScale / 1.0f;
+        scalingHidde = true;
+    }
+    
+    void updateRotate()
+    {
+        if (!rotationHidde && !rotationShow && !scalingHidde)
+            return;
+        
+        time += Time.deltaTime;
+        if (time > ((scalingHidde) ? timeScale : timeRotation))
+            time = (scalingHidde)?timeScale:timeRotation;
+
+        float value = time * ((scalingHidde)?1.0f:180.0f) / ((scalingHidde) ? timeScale : timeRotation); 
+
+        if (rotationHidde)
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 180.0f - value, 0.0f));
+
+        if (rotationShow)
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0.0f, value, 0.0f));
+
+        if (scalingHidde)
+            gameObject.transform.localScale = new Vector3(1.0f - value, 1.0f - value, 0.0f);
+        
+        if ( (value >= 180.0f && !scalingHidde) || (value >= 1.0f && scalingHidde))        
+            rotationShow = rotationHidde = scalingHidde = false;  
+
+    }
+
+    void OnMouseUp()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);     
+        Collider2D collider = Physics2D.OverlapPoint(mousePosition);
+
+        if (collider && collider.gameObject == gameObject && !orientation)
+            managger.OnClickCarta(this);        
+    }
 }
---
+++ Proyecto Unity 2D/Assets/scripts/memoryGame/MemoryGameMannager.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MemoryGameMannager : MonoBehaviour 
+{
+    public Vector2 Piezas = new Vector2(4, 4);
+    public Sprite SpriteBack;
+    public Sprite[] Sprites;
+    public GameObject CartaPrefab;
+    public float TiempoEsperaInicio = 3.0f;
+    public float RetardoEfecto = 0.05f;
+    public float TimpoEsperaResolver = 0.5f;
+    public float TimpoRotacionCarta = 0.5f;
+    public float TimpoScalingCarta= 0.5f;
+    public string NameSceneEndGame = "meun";
+
+    //! Private
+    private int maxNumber = 0;
+    private Vector2 CartaSize = Vector2.zero;
+    private List<int> ids = new List<int>();
+
+    private List<Carta> cartSelected = new List<Carta>();
+    private int total = 0;
+
+	void Start () 
+    {
+        maxNumber = (int)( (Piezas.x * Piezas.y) * 0.5f);
+            
+        if ((Piezas.x * Piezas.y)%2.0f > 0.0f)
+        {
+            Debug.Log("Matiz impar, ejmplos validos 2x2,2x3,4x4,4x2,8x8");
+            return;
+        }
+
+        if (Sprites.Length > 0)
+            CartaSize = Sprites[0].bounds.size;
+
+        // Crea la lista de ids
+        for (int i = 0; i < Sprites.Length; i++) 
+            ids.Add(i);
+
+        // Shuffle Lista
+        Shuffle(ids);
+        ids.RemoveRange(maxNumber, ids.Count - maxNumber);
+        ids.AddRange(ids.GetRange(0, ids.Count));
+        Shuffle(ids);    
+
+        int index = 0;
+        for (int row = 0; row < Piezas.x; row++)
+        {
+            for (int col = 0; col < Piezas.y; col++)
+            {
+                GameObject carta =  Instantiate(CartaPrefab) as GameObject;
+                carta.name = "Carta " + index.ToString();
+                
+                carta.transform.position = gameObject.transform.position +
+                    new Vector3(col * CartaSize.x, row * CartaSize.y * -1);                
+
+                carta.transform.parent = gameObject.transform;
+
+                Carta scriptCarta = carta.GetComponent<Carta>();
+                scriptCarta.managger = this;
+                scriptCarta.id = ids[index];
+                scriptCarta.timeWait = TiempoEsperaInicio + index * RetardoEfecto;
+                scriptCarta.timeRotation = TimpoRotacionCarta;
+                scriptCarta.timeScale = TimpoScalingCarta;
+                scriptCarta.SpriteBack = SpriteBack;
+
+                SpriteRenderer spriteRenderCartaFront = scriptCarta.front.gameObject.GetComponent<SpriteRenderer>();
+                spriteRenderCartaFront.sprite = Sprites[ids[index]];  
+                
+                SpriteRenderer spriteRenderCartaBack = scriptCarta.back.gameObject.GetComponent<SpriteRenderer>();
+                spriteRenderCartaBack.sprite = SpriteBack;
+
+                index++;
+            }
+        }
+	}
+
+    void Update()
+    {
+        if (total == maxNumber)
+        {
+            Invoke("loadLevel", TimpoScalingCarta);
+            total = 0;
+        }
+    }
+		
+        
+    public void loadLevel()  
+    {
+        Application.LoadLevel(NameSceneEndGame);
+	}
+
+    public void OnClickCarta(Carta carta)
+    {
+        if (cartSelected.Count == 2)
+            return;
+
+        carta.rotateShow();
+        cartSelected.Add(carta);
+
+        if (cartSelected.Count < 2)
+            return;
+
+        Invoke("resolver", TimpoEsperaResolver);              
+    }
+
+    public void resolver()
+    {
+        if (cartSelected[0].id == cartSelected[1].id)
+        {
+            cartSelected[0].scaleHidde();
+            cartSelected[1].scaleHidde();
+            total++;
+        }
+        else
+        {
+            cartSelected[0].rotateHidde();
+            cartSelected[1].rotateHidde();
+        }
+
+        cartSelected.Clear();  
+    }
+
+    void Shuffle<T>(List<T> array)
+    {
+        int n = array.Count;
+        for (int i = 0; i < n; i++)
+        {
+            int r = Random.Range(0, n - 1);  //i + (int)(Random.value * (n - i));
+            T t = array[r];
+            array[r] = array[i];
+            array[i] = t;
+        }
+    }
+
+}
